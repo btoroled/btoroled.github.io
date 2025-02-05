e70447221f1efa722bb80e52c1bec0347da9ece8
elbull-tarhur <benja.toro.l98@gmail.com>
2025-02-05 15:45:52 -0500

    Se termino el HTML con todas sus tags.

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+{
+    "loop": true,
+    "speed": 600,
+    "autoplay": {
+      "delay": 5000
+    },
+    "slidesPerView": "auto",
+    "pagination": {
+      "el": ".swiper-pagination",
+      "type": "bullets",
+      "clickable": true
+    },
+    "breakpoints": {
+      "320": {
+        "slidesPerView": 1,
+        "spaceBetween": 40
+      },
+      "1200": {
+        "slidesPerView": 3,
+        "spaceBetween": 1
+      }
+    }
+  }
